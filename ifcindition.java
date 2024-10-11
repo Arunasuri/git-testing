acbd62814cca733c3998015e394f960fa3058ac9
Aruna <suri1aruna@gmail.com>
2024-10-11 10:48:05 -0400

    condition

---
+++ ifcindition.java
@@ -0,0 +1,12 @@
+package oct10th2024;
+
+public class ifcindition {
+    public static void main(String args[])
+    {
+        int x=20;
+        if(x>=10&&x<=25)
+        {
+            System.out.println("x is bigger than 10");
+        }
+    }
+}
